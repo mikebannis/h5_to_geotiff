ca0ab205d6865e54ec7600108a7c9fc340ee4cbd
mbannist <mike.bannister@nrel.gov>
2024-01-03 11:02:21 -0700

    Use setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    install_requires = f.readlines()
+
+setup(
+    name='h5_to_geotiff',
+    author='Mike Bannister',
+    version='0.0.1',
+    entry_points={
+        'console_scripts': ['h5-to-geotiff=h5_to_geotiff.h5_to_geotiff:main']
+    },
+    install_requires=install_requires,
+)
